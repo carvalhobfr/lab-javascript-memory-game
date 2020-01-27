ac913ba07ddb19a2b0bd3006451601fe10dabb58
root <root@DESKTOP-MRC.localdomain>
2020-01-27 18:07:56 +0000

    v1

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,15 +1,27 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // Add the rest of the properties here
   }
 
   shuffleCards() {
-    // ...
+
+    Math.random(this.cards);
   }
 
   checkIfPair(card1, card2) {
-    // ...
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    else {
+      return false
+    };
+
   }
 
   isFinished() {
